0f435f6a1345cceb6bb3dc6fbacb7cbec1833522
Prajakta Sumbe <sumbe.p@husky.neu.edu>
2019-02-05 00:07:30 -0500

    Added custom_stopwords implementation

--- DataExtraction/fintech_textrank.py
+++ DataExtraction/fintech_textrank.py
@@ -8,7 +8,7 @@ import time
 # in_file.close()
 
 # give file path to store data  in variable
-filename = 'noisy_text_data.txt'
+filename = 'final_data.txt'
 file = open(filename, 'rt',encoding="UTF-8")
 text = file.read()
 file.close()
@@ -30,21 +30,23 @@ stripped = [w.translate(table) for w in tokens]
 # remove remaining tokens that are not alphabetic
 words = [word for word in stripped if word.isalpha()]
 # filter out stop words
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english'))
-words = [w for w in words if not w in stop_words]
+# from nltk.corpus import stopwords
+# stop_words = stopwords.words('english')
+
+import custom_stopwords
+
+# print (custom_stopwords.stop_words_list)
+words = [w for w in words if not w in custom_stopwords.stop_words_list]
 
 
 
 new_txt = " ".join(word for word in words)
 
-# print(new_txt)
 
 
-start=time.time()
+# start=time.time()
 phrase,word=pyt.top_keywords_sentences(new_txt,phrase_limit=100)
 
-# print('Keywords:',word)
 
 import csv
 with open('Fintech_textrank_words.csv', 'w',encoding="UTF-8") as csvFile:
@@ -54,5 +56,5 @@ with open('Fintech_textrank_words.csv', 'w',encoding="UTF-8") as csvFile:
 csvFile.close()
 
 
-# print('Keywords:',text)
+print('Keywords:',word)
 #print('Time Taken: ',time.time()-start)
