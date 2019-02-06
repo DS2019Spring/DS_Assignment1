63cf8e518bb70502251cc9a822e9591582a5317d
suthardhaval24 <suthardhaval24@gmail.com>
2019-02-05 23:25:03 -0500

    Scrapping extraction done

--- Web scrapping/region_scrapper.py
+++ Web scrapping/region_scrapper.py
@@ -10,6 +10,12 @@ from selenium.webdriver.common.keys import Keys
 import re
 import sys
 import time
+import requests
+import logging
+import numpy as np
+from array import *
+
+logger = logging.getLogger(__name__)
 
 
 # In[2]:
@@ -19,6 +25,7 @@ driver = webdriver.Chrome(executable_path)
 driver.get('https://regions.wd5.myworkdayjobs.com/Regions_Careers')
 time.sleep(10)
 pause=2
+'''
 lastHeight = driver.execute_script("return document.body.scrollHeight")
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -27,13 +34,19 @@ while True:
     if newHeight == lastHeight:
         break
     lastHeight = newHeight
+'''
 html = driver.page_source
 soup = BeautifulSoup(html,"lxml")
 li_class = soup.findAll("li", { "class" : "WIYF WK3N WE5 WP-F" })
 base_url = "https://regions.wd5.myworkdayjobs.com/en-US/Regions_Careers/job/"
+
+#to get the list o urls
+final_url_list = []
+final_array = [[]]
+
 for s in li_class:
     a = s.findAll("div",{"class","gwt-Label WOTO WISO"})[0].string
-    c = re.sub("[^a-zA-Z]","-",str(a))
+    c = re.sub("[^a-zA-Z0-9]","-",str(a))
     b = s.findAll("span",{"class","gwt-InlineLabel WM-F WLYF"})[0].string
     try:
         d= b.split(" | ")[0]
@@ -44,29 +57,97 @@ for s in li_class:
         d='null'
         e='null'
     final_url = base_url+d+"/"+c+"_"+e
-    print(final_url)
-
-
-# In[5]:
-
-
-
-
-
-# In[ ]:
+    #print(final_url)
+    #append all the urls
+    final_url_list.append(final_url)    
+driver.close()
 
 
 
 
 
-# In[ ]:
+#extract data from csv file into list : panda is a good library to read csv
+
+import pandas as py
+colname = ['rowid','words']
+
+file_data = py.read_csv('Fintech_TFIDF_Final.csv',names = colname)
+
+file_word = file_data.words.tolist()
+
+# print(file_word)
+
+#initialize an empty dictionary for storing an information 
+
+final_dictionary = []
+word_dictionary= []
+
+i = 0 #for generating rowid for jobs
+#for generating rowid for word_doc
+counter = 0 #for countring occurences of the words in the list
+#go to each url and get the list of words 
+
+for url in final_url_list[:4]:
+    dict_count ={}
+    j=0
+    try:
+        
+        final_string1=""
+        #print(url)
+        driver = webdriver.Chrome(executable_path)
+        driver.get(url)
+        time.sleep(5)
+        html = driver.page_source
+        soup=BeautifulSoup(html,"lxml")
+        body = soup.findAll("div",{"class","GWTCKEditor-Disabled"})[1]
+        body1 = str(body).replace("<span><span>","")
+        body2 = str(body1).replace("</span></span>","")
+        for text in BeautifulSoup(body2,"lxml").findAll("li"):
+            text1 = BeautifulSoup(str(text),"lxml")
+            text2 = text1.find('li').get_text()
+            final_string1 += text2
+        
+        for text3 in BeautifulSoup(body2,"lxml").findAll("p"):
+            text4 = BeautifulSoup(str(text3),"lxml")
+            text5 = text4.find('p').get_text()
+            final_string1 += text5
+
+        url_body = final_string1.split()
+        # print(url_body)
+        num=100 #count for j
+
+        for key_word in file_word :
+            dict_count[key_word] = 0
+
+        for word in url_body :
+            if word in dict_count :
+                dict_count[word] += 1
+            else :
+                pass
+        
+        
+        f_list=list(dict_count.values())
+        final_array.insert(i,f_list)
 
 
+        # for words in url_body:
+        #     i+=1
+        #     for w in file_word[1:101]:
+        #         j+=1
+        #         temp = { j : w}
+        #         word_dictionary.append(temp.copy())
+        #     j=0
+        temp1 = {"job no" : i, "institution" : "Regios",
+                "listid": i, "url" : url, "list" : i, "words": dict_count}   
+            # final_dictionary.append(temp1.copy())   
+        final_dictionary.append(temp1.copy())  
+        print(dict_count)
+        
+        driver.close()
+        i+=1
 
+    except:
+        logger.exception("Not parsable URL")
+        continue    
 
-
-# In[ ]:
-
-
-
-
+print(final_array)
